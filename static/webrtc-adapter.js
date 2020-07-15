c3103f9a20275ea5e729a532b33d3498a86534f3
Logan Keenan <d.logankeenan@gmail.com>
2020-07-14 20:44:43 -0500

    refactor webrtc adapter to make it a bit more readable

--- static/webrtc-adapter.js
+++ static/webrtc-adapter.js
@@ -1,10 +1,29 @@
-import {
-    webrtc_on_message,
-    webrtc_on_connect,
-    webrtc_on_signal,
-    app,
-    AppRequest
-} from '/node_modules/@logankeenan/shared-space-app/shared_space_app.js';
+import {app, AppRequest, webrtc_on_connect} from '/node_modules/@logankeenan/shared-space-app/shared_space_app.js';
+
+async function processIncomingData(data, peer) {
+    let dataAsJson = JSON.parse(data);
+
+    if (dataAsJson.type === "request") {
+        var webrtcRequest = JSON.parse(dataAsJson.message);
+
+        let appRequest = new AppRequest(webrtcRequest.path, webrtcRequest.method);
+        appRequest.body = webrtcRequest.body;
+
+        let appResponse = await app(appRequest);
+
+        peer.send(JSON.stringify({
+            type: 'response',
+            request_id: dataAsJson.request_id,
+            message: appResponse.as_json_string()
+        }));
+
+    } else if (dataAsJson.type === "response") {
+        const resolver = window.simplePeerAdapter.activeRequestsResolver[dataAsJson.request_id];
+        resolver(dataAsJson.message);
+
+        delete window.simplePeerAdapter.activeRequestsResolver[dataAsJson.request_id];
+    }
+}
 
 function setup() {
 
@@ -12,13 +31,9 @@ function setup() {
         peers: {},
         activeRequestsResolver: {},
         createSimplePeer: async function (initiator, device_id, offer) {
-            return new Promise((resolve) => {
+            return new Promise((resolveCreatePeerConnection) => {
                 let peer = new SimplePeer({
                     initiator: initiator === "true"
-                });
-                var connectionPromiseResolve;
-                var connectionPromise = new Promise((resolve) => {
-                    connectionPromiseResolve = resolve;
                 });
 
                 if (offer !== "") {
@@ -30,8 +45,6 @@ function setup() {
                 var signalData = [];
                 window.simplePeerAdapter.peers[device_id] = {
                     connection: peer,
-                    connectionPromise,
-                    connectionPromiseResolve,
                     signalData,
                 };
 
@@ -47,41 +60,15 @@ function setup() {
                 })
 
                 peer.on('connect', () => {
-                    connectionPromiseResolve();
-
                     webrtc_on_connect(device_id);
                 });
 
                 peer.on('data', async data => {
-
-                    let parsedData = JSON.parse(data);
-
-                    if (parsedData.type === "request") {
-                        var request = JSON.parse(parsedData.message);
-
-                        let app_request = new AppRequest(request.path, request.method);
-                        app_request.body = request.body;
-
-                        let response = await app(app_request);
-                        // TODAY - this needs to be a json message so I can send it
-
-                        let chunk = JSON.stringify({
-                            type: 'response',
-                            request_id: parsedData.request_id,
-                            message: response.as_json_string()
-                        });
-                        peer.send(chunk);
-
-                    } else if (parsedData.type === "response") {
-                        const resolver = window.simplePeerAdapter.activeRequestsResolver[parsedData.request_id];
-                        resolver(parsedData.message);
-
-                        delete window.simplePeerAdapter.activeRequestsResolver[parsedData.request_id];
-                    }
+                    await processIncomingData(data, peer);
                 });
 
                 setTimeout(function () {
-                    resolve(JSON.stringify(signalData));
+                    resolveCreatePeerConnection(JSON.stringify(signalData));
                 }, 3000);
             });
         },
@@ -93,11 +80,12 @@ function setup() {
             });
         },
         sendSimplePeerMessage: function (message, device_id) {
-            let promise = new Promise((resolve) => {
+            return new Promise((resolve) => {
                 let peer = window.simplePeerAdapter.peers[device_id];
                 let request_id = uuidv4();
 
                 window.simplePeerAdapter.activeRequestsResolver[request_id] = resolve;
+
                 let chunk = JSON.stringify({
                     type: 'request',
                     request_id,
@@ -106,8 +94,6 @@ function setup() {
                 });
                 peer.connection.send(chunk);
             });
-
-            return promise;
         }
     }
 }
